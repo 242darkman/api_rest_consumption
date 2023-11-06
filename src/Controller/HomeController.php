c3dcf1cf244a0bff6e92c9520079fd246ed51ea6
242darkman <53907600+242darkman@users.noreply.github.com>
2023-11-06 19:00:36 +0100

    feat: ajout d'un service pour consommer l'api de recherche d'entreprise et affichage des résultats

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\EnterpriseApiConsumption;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request, EnterpriseApiConsumption $entrepriseApiConsumption): Response
     {
+        $searchTerm = $request->query->get('search', '');
+        $enterpriseResults = [];
+
+        if ($searchTerm !== "") {
+            $enterpriseResults = $entrepriseApiConsumption->search($searchTerm);
+        }
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'search_term' => $searchTerm,
+            'enterprises' => $enterpriseResults['results'],
         ]);
     }
 }
--- src/Service/EnterpriseApiConsumption.php
+++ src/Service/EnterpriseApiConsumption.php
@@ -1,16 +1,36 @@
 <?php
 
-//namespace app\Service;
+namespace App\Service;
 
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class EnterpriseApiConsumption
 {
-
   private $client;
 
   public function __construct(HttpClientInterface $client)
   {
     $this->client = $client;
+  }
+
+  public function search(string $query): array
+  {
+    $response = $this->client->request(
+      'GET',
+      'https://recherche-entreprises.api.gouv.fr/search',
+      [
+        'query' => [
+          'q' => $query,
+          'per_page' => 10,
+          'page' => 1,
+        ],
+      ]
+    );
+
+    if (200 !== $response->getStatusCode()) {
+      throw new \Exception('Failed to retrieve data from the API.');
+    }
+
+    return $response->toArray();
   }
 }
