ae1cea963f5641484622d27126bcbb13593a4d96
242darkman <53907600+242darkman@users.noreply.github.com>
2023-11-06 17:06:56 +0100

    ajout du formulaire de recherche (bug présent)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'search')]
+    public function index(Request $request): Response
     {
+        //$searchTerm = $request->request->get('search', '');
+
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'form' => $form->createView(),
         ]);
     }
 }
