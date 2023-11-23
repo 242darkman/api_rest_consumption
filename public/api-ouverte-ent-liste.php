b3898438eec71428a6f2863818d68b99cb85ca4d
242darkman <53907600+242darkman@users.noreply.github.com>
2023-11-23 19:16:11 +0100

    fix: refactorisation du code pour une meilleure lisibilité et maintenabilité

--- public/api-ouverte-ent-liste.php
+++ public/api-ouverte-ent-liste.php
@@ -1,50 +1,98 @@
 <?php
 
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-
-        $format = $_GET['format'];
-
-        // Vérifier le format demandé
-        if (!in_array($format, ['json', 'csv'])) {
-            header('HTTP/1.1 406 Not Acceptable');
-            echo "erreur 406";
-            exit;
-        }
-
-         // Lecture du fichier qui contient les entreprises
-        $content = file_get_contents('enterprises.txt'); //
-        
-        // Vérifier si le fichier est vide
-        if (filesize('enterprises.txt') > 0) {
-           
-            // Envoyer le contenu au format JSON
-            if ($format === 'json') {
-                header('Content-Type: application/json');
-                echo $content;
-            }
-            elseif ($format === 'csv') {
-                header('Content-Type: text/csv');
-                
-                // Convertir le JSON en tableau associatif
-                $enterpriseList = json_decode($content, true);
-        
-                // Écrire les en-têtes CSV
-                echo "siren,siret,Raison_sociale,Adresse_Num,Adresse_Voie,Adresse_Code_postal,Adresse_Ville,Geo_adresse,GPS_Latitude,GPS_Longitude\n";
-
-                // Écrire le contenu CSV
-                foreach ($enterpriseList['save_enterprises'] as $enterprise) {
-                    echo "{$enterprise['siren']},{$enterprise['siret']},{$enterprise['Raison_sociale']},{$enterprise['Adresse']['Num']},{$enterprise['Adresse']['Voie']},{$enterprise['Adresse']['Code_postal']},{$enterprise['Adresse']['Ville']},{$enterprise['Adresse']['Geo_adresse']},{$enterprise['Adresse']['GPS']['Latitude']},{$enterprise['Adresse']['GPS']['Longitude']}\n";
-                }
-            }
-        }
-        else{
-            echo "Aucune entreprise trouvée, la liste est vide";
-            exit;
-        }
-    } else {
-        header('HTTP/1.1 405 Method Not Allowed');
-        echo "Erreur 405 : Méthode non autorisée";
-        exit();
+/**
+ * Handle the GET request
+ */
+function handleGetRequest($format) {
+    if (!isValidFormat($format)) {
+        sendErrorResponse(406, "Format non valide");
+        return;
     }
+
+    $content = file_get_contents('enterprises.txt');
+
+    if (filesize('enterprises.txt') <= 0) {
+        echo "Aucune entreprise trouvée, la liste est vide";
+        return;
+    }
+
+    switch ($format) {
+        case 'json':
+            sendJsonResponse($content);
+            break;
+        case 'csv':
+            sendCsvResponse($content);
+            break;
+    }
+}
+
+/**
+ * Check if the format is valid
+ */
+function isValidFormat($format) {
+    return in_array($format, ['json', 'csv']);
+}
+
+/**
+ * Send a JSON response
+ */
+function sendJsonResponse($content) {
+    header('Content-Type: application/json');
+    echo $content;
+}
+
+/**
+ * Send a CSV response
+ */
+function sendCsvResponse($content) {
+    header('Content-Type: text/csv');
+    $enterpriseList = json_decode($content, true);
+    outputCsvHeaders();
+    outputCsvContent($enterpriseList['save_enterprises']);
+}
+
+/**
+ * Output CSV headers
+ */
+function outputCsvHeaders() {
+    echo "siren,siret,Raison_sociale,Adresse_Num,Adresse_Voie,Adresse_Code_postal,Adresse_Ville,Geo_adresse,GPS_Latitude,GPS_Longitude\n";
+}
+
+/**
+ * Output CSV content
+ */
+function outputCsvContent($enterprises) {
+    foreach ($enterprises as $enterprise) {
+        echo implode(',', [
+            $enterprise['siren'],
+            $enterprise['siret'],
+            $enterprise['Raison_sociale'],
+            $enterprise['Adresse']['Num'],
+            $enterprise['Adresse']['Voie'],
+            $enterprise['Adresse']['Code_postal'],
+            $enterprise['Adresse']['Ville'],
+            $enterprise['Adresse']['Geo_adresse'],
+            $enterprise['Adresse']['GPS']['Latitude'],
+            $enterprise['Adresse']['GPS']['Longitude']
+        ]) . "\n";
+    }
+}
+
+/**
+ * Send an error response
+ */
+function sendErrorResponse($statusCode, $message) {
+    header("HTTP/1.1 $statusCode");
+    echo $message;
+}
+
+/**
+ * Check if the request method is GET
+ */
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    handleGetRequest($_GET['format']);
+} else {
+    sendErrorResponse(405, "Erreur 405 : Méthode non autorisée");
+}
 
 ?>
