a639851e4785eadc58349cd6195f510a4a0f3672
242darkman <53907600+242darkman@users.noreply.github.com>
2023-11-23 20:48:09 +0100

    fix: refactorisation du code pour une meilleure lisibilité et maintenabilité pour le fichier `api-ouverte-ent.php` permettant de récupérer une entreprise spécifique

--- public/api-ouverte-ent.php
+++ public/api-ouverte-ent.php
@@ -1,64 +1,97 @@
 <?php
 
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        // Récupérer le SIREN de l'entreprise depuis la requête
-        $siren = isset($_GET['siren']) ? $_GET['siren'] : null;
+use Exception as RequestException;
 
-        // Vérifier si le SIREN est fourni
-        if ($siren === null) {
-            header('HTTP/1.1 400 Bad Request');
-            echo "Erreur 400 : Le paramètre 'siren' est requis.";
-            exit;
-        }
+/**
+ * Handles the GET request from the user
+ */
+function handleGetRequest($get) {
+    $siren = getSirenParameter($get);
 
-        $content = file_get_contents('enterprises.txt');
-        
-        if (filesize('enterprises.txt') > 0) {
-
-            if (!$content) {
-                header('HTTP/1.1 500 Internal Server Error');
-                echo "Erreur 500 : Problème lors de l'ouverture du fichier";
-                exit;
-            }
-    
-            $enterpriseList = json_decode($content, true);
-    
-            if ($enterpriseList === null) {
-                header('HTTP/1.1 500 Internal Server Error');
-                echo "Erreur 500 : Problème lors de la conversion du contenu en JSON";
-                exit;
-            }
-    
-            // Rechercher l'entreprise avec le SIREN spécifié
-            $found = false;
-            $selectedEnterprise = null;
-            foreach ($enterpriseList['save_enterprises'] as $enterprise) {
-                if ($enterprise['siren'] === $siren) {
-                    $found = true;
-                    $selectedEnterprise = $enterprise;
-                    break;
-                }
-            }
-    
-            // Si l'entreprise avec le SIREN est trouvée, retourner les informations au format JSON
-            if ($found) {
-                header('HTTP/1.1 200 OK');
-                header('Content-Type: application/json');
-                echo json_encode($selectedEnterprise, JSON_PRETTY_PRINT);
-            } else {
-                header('HTTP/1.1 404 Not Found');
-                echo "Erreur 404 : Aucune entreprise trouvée.";
-            }
-        }
-        else{
-            echo "Aucune entreprise trouvée, la liste est vide";
-            exit;
-        }
-
-    } else {
-        // Méthode HTTP non autorisée
-        header('HTTP/1.1 405 Method Not Allowed');
-        echo "Erreur 405 : Méthode non autorisée";
-        exit();
+    if (!$siren) {
+        sendErrorResponse(400, "Erreur 400 : Le paramètre 'siren' est requis.");
+        return;
     }
+
+    $content = readEnterpriseFile();
+    if ($content === false) {
+        sendErrorResponse(500, "Erreur 500 : Problème lors de l'ouverture du fichier");
+        return;
+    }
+
+    $enterpriseList = decodeEnterpriseContent($content);
+    if ($enterpriseList === null) {
+        sendErrorResponse(500, "Erreur 500 : Problème lors de la conversion du contenu en JSON");
+        return;
+    }
+
+    findAndSendEnterprise($enterpriseList, $siren);
+}
+
+/**
+ * Retrieves the 'siren' parameter from the GET request
+ */
+function getSirenParameter($get) {
+    return isset($get['siren']) ? $get['siren'] : null;
+}
+
+/**
+ * Reads content from the enterprises file
+ */
+function readEnterpriseFile() {
+    return filesize('enterprises.txt') > 0 ? file_get_contents('enterprises.txt') : false;
+}
+
+/**
+ * Decodes the JSON content of the file
+ */
+function decodeEnterpriseContent($content) {
+    return json_decode($content, true);
+}
+
+/**
+ * Finds and sends the enterprise information if available
+ */
+function findAndSendEnterprise($enterpriseList, $siren) {
+    foreach ($enterpriseList['save_enterprises'] as $enterprise) {
+        if ($enterprise['siren'] === $siren) {
+            sendEnterpriseResponse($enterprise);
+            return;
+        }
+    }
+    echo "Aucune entreprise trouvée, la liste est vide";
+}
+
+/**
+ * Sends an error response with a specific status code and message
+ */
+function sendErrorResponse($statusCode, $message) {
+    header("HTTP/1.1 $statusCode");
+    echo $message;
+    throw new RequestException($message);
+}
+
+/**
+ * Sends the enterprise information as a JSON response
+ */
+function sendEnterpriseResponse($enterprise) {
+    header('HTTP/1.1 200 OK');
+    header('Content-Type: application/json');
+    echo json_encode($enterprise, JSON_PRETTY_PRINT);
+}
+
+/**
+ * Check the request method, and process accordingly
+ */
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    try {
+        handleGetRequest($_GET);
+    } catch (RequestException $e) {
+        echo 'Une erreur est survenue. Veuillez réessayer plus tard.';
+        error_log($e->getMessage());
+    }
+} else {
+    sendErrorResponse(405, "Erreur 405 : Méthode non autorisée");
+}
+
 ?>
